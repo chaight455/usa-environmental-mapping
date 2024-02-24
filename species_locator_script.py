20ad45804af7243695408e7f0f77815cff48f7a5
chaight455 <149899080+chaight455@users.noreply.github.com>
2024-02-24 16:24:31 -0600

    Add files via upload

--- species_locator_script.py
+++ species_locator_script.py
@@ -6,17 +6,15 @@ import earthpy as et
 import pgeocode
 import streamlit_folium
 import pandas as pd
-import os
-
-#dataframe
-county_data = pd.read_csv(os.path.join("county_info_2016.csv"))
 
 #Functions
 def pinpoint_location(zipcode = "0"):
       nomi = pgeocode.Nominatim('us')
+      #Check if zipcode is valid
       if (zipcode != "0" and len(zipcode) != 5):
             st.write("Not a Valid US Zipcode!")
             return folium.Map(location=[37.0902, -95.7129], zoom_start = 4)
+      #display map based on latiitude and longitude
       if (zipcode != "0"):
             user_zip = nomi.query_postal_code(zipcode)
             user_lat = user_zip['latitude']
@@ -29,15 +27,15 @@ def pinpoint_location(zipcode = "0"):
             mymap = folium.Map(location=[37.0902, -95.7129], zoom_start = 4)
       return mymap
 
-#Streamlit output 
+#Print out blank usa map
+streamlit_folium.folium_static(pinpoint_location(str(zipcode)))
+
+#get zip from user
 zipcode = st.number_input('Enter Valid 5-digit US Zipcode to See Specific Location', min_value=0, max_value=99999, value=0)
 
+#get map type from user
 dropdown = st.selectbox("Choose Map", ["Droughts"])
 
-if dropdown == "Droughts":
-      mymap = pinpoint_location(str(zipcode)) # Store the map returned by the function
-      for index, row in county_data.iterrows():
-          # Access the 'INTPTLAT' and 'INTPTLONG' columns directly
-          folium.Marker([row["INTPTLAT"], row[9]], popup=row["NAME"]).add_to(mymap)
-    
-streamlit_folium.folium_static(mymap)
+#add markers to map based on type that user choose
+if (dropdown == "Droughts"):
+      streamlit_folium.folium_static(pinpoint_location(str(zipcode)))
