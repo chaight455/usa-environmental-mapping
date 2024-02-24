5f1a494344034dd311076f59eee2812e21abe114
chaight455 <149899080+chaight455@users.noreply.github.com>
2024-02-24 15:07:57 -0600

    Add files via upload

---
+++ species_locator_script.py
@@ -0,0 +1,34 @@
+#imports
+import streamlit as st
+import folium
+import geopandas as gpd
+import earthpy as et
+import pgeocode
+import streamlit_folium
+import pandas as pd
+
+#Functions
+def pinpoint_location(zipcode = "0"):
+      nomi = pgeocode.Nominatim('us')
+      if (zipcode != "0" and len(zipcode) != 5):
+            st.write("Not a Valid US Zipcode!")
+            return folium.Map(location=[37.0902, -95.7129], zoom_start = 4)
+      if (zipcode != "0"):
+            user_zip = nomi.query_postal_code(zipcode)
+            user_lat = user_zip['latitude']
+            user_long = user_zip['longitude']
+            if (pd.isnull(user_lat)):
+                  st.write("Not a Valid US Zipcode!")
+                  return folium.Map(location=[37.0902, -95.7129], zoom_start = 4)
+            mymap = folium.Map(location=[user_lat, user_long], zoom_start=12)
+      else:
+            mymap = folium.Map(location=[37.0902, -95.7129], zoom_start = 4)
+      return mymap
+
+#Streamlit output 
+zipcode = st.number_input('Enter Valid 5-digit US Zipcode to See Specific Location', min_value=0, max_value=99999, value=0)
+
+dropdown = st.selectbox("Choose Map", ["Droughts"])
+
+
+streamlit_folium.folium_static(pinpoint_location(str(zipcode)))
